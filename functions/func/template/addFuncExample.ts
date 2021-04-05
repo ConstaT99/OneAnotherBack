6500b4bd75fbbda517f07dd144dc40568d465c2f
Tao, Jerry <rtao6@illinois.edu>
2021-04-04 23:12:37 -0500

    update file structure

--- functions/func/template/addFuncExample.ts
+++ functions/func/template/addFuncExample.ts
@@ -1,17 +1,17 @@
-import * as functions from "firebase-functions";
-import {db} from "../db";
+import * as functions from 'firebase-functions';
+import { db } from '../../db';
 
 /* This is a Sample Create Function for Cloud function development */
 export const addExample = async (data:{
   sampleId: string;
   text: string;
-})=> {
+}) => {
   // const { sampleId, text} = data; // get value in data.
-  const collection = "test";
-  functions.logger.info("this is add Example Function");
+  const collection = 'test';
+  functions.logger.info('this is add Example Function');
   const testRef = db.collection(collection);
   // this will return ref
-  return await testRef.add(data);
+  return testRef.add(data);
 };
 
 export default functions.https.onCall(addExample);
--- functions/func/template/helloWorld.ts
+++ functions/func/template/helloWorld.ts
@@ -1,8 +1,9 @@
-import * as functions from "firebase-functions";
-// import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions';
+/* This is file contain function display Hello World in the web */
 
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from OneAnother!");
+const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from OneAnother!');
 });
+
+export default helloWorld;
