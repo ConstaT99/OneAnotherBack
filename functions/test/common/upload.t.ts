437eb73604320f338e14cb902638c4787eeae058
Tao, Jerry <rtao6@illinois.edu>
2021-05-04 21:33:31 -0500

    save the progress

---
+++ functions/test/common/upload.t.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import { addVideo } from '../../func/video/addVideoFunc';
+// import { readVideo } from '../../func/video/readVideoFunc';
+// import { deleteVideo } from '../../func/video/deleteVideoFunc';
+// import { updateVideo } from '../../func/video/updateVideoFunc';
+import 'mocha';
+import * as testinfo from '../testInfo/testInfo';
+
+describe('Uplaod File Test', () => {
+  it('up;aod file test', async () => {
+
+  });
+});
