89b390d7ed7ea5fd8e75ffd742f5cef58cf41003
yihjian <yihongj2@illinois.edu>
2021-08-11 21:07:15 -0500

    reword some comments and add more extensive test for users

--- functions/test/user/userReadTest.ts
+++ functions/test/user/userReadTest.ts
@@ -3,16 +3,21 @@ import { readUser } from '../../func/user/readUserFunc';
 import 'mocha';
 
 describe('User read test', () => {
-  it('read user func return right result that test inserted', async () => {
-    const testdata = {
-      uid: 'Nbcl2gT2XkQpSaO4gnAcJAiS0YF3',
-    };
+  const testdata = {
+    uid: 'CmSdt5xeSKfZiAw84ye1PC8zOjf2',
+  };
+
+  it('should read successfully for existing user', async () => {
     const userdata = await readUser(testdata);
     if (userdata === undefined) {
-      console.log('user is undefined');
-      expect(false).to.equal(true); // A stupid test statement
+      expect.fail('user is undefined'); // A stupid test statement
     } else {
-      console.log('the user email is ', userdata.email);
+      expect(userdata.email).to.equal('trnjerrycostantine@gmail.com');
     }
+  });
+
+  it('should get undefined for non-existing user', async () => {
+    testdata.uid = '666';
+    expect(await readUser(testdata)).to.equal(undefined);
   });
 });
--- functions/test/user/userUpdateTest.ts
+++ functions/test/user/userUpdateTest.ts
@@ -4,18 +4,68 @@ import { updateUser } from '../../func/user/updateUserFunc';
 import 'mocha';
 
 describe('User update test', () => {
-  it('read user func return right result that test inserted', async () => {
-    const testdata = {
-      uid: 'Nbcl2gT2XkQpSaO4gnAcJAiS0YF3',
-      updateField: 'userName',
-      updateContext: 'Constantine T',
-    };
-    await updateUser(testdata);
-    const userdata = await readUser(testdata);
+  const testdata = {
+    uid: 'CmSdt5xeSKfZiAw84ye1PC8zOjf2',
+    updateField: 'userName',
+    updateContext: `abc123\r${Date()}`,
+  };
+
+  it('should update user name successfully', async () => {
+
+    // First we track old name
+    let userdata = await readUser(testdata);
     if (userdata === undefined) {
-      return false;
+      expect.fail('user is undefined');
+    }
+    const oldUserName = userdata.userName;
+
+    // We then update and check if the name is updated properly
+    await updateUser(testdata);
+    userdata = await readUser(testdata);
+    if (userdata === undefined) {
+      expect.fail('user is undefined');
     }
     expect(userdata.userName).to.equal(testdata.updateContext);
-    return 0;
+
+    // Restore original name
+    testdata.updateContext = oldUserName;
+    await updateUser(testdata);
+    userdata = await readUser(testdata);
+    if (userdata === undefined) {
+      expect.fail('user is undefined');
+    }
+    expect(userdata.userName).to.equal(oldUserName);
+  });
+
+  it('should reject null field', async () => {
+    // @ts-ignore
+    testdata.updateField = null;
+    await updateUser((testdata))
+      .then(() => {
+        expect.fail('Not suppose to update for null context');
+      })
+      .catch(() => {
+      });
+  });
+
+  it('should reject non existing field', async () => {
+
+    testdata.updateField = '';
+
+    await updateUser((testdata))
+      .then(() => {
+        expect.fail('Not suppose to update for empty field');
+      })
+      .catch(() => {
+      });
+
+    testdata.updateField = 'LOL';
+
+    await updateUser((testdata))
+      .then(() => {
+        expect.fail('Not suppose to update for non existing field');
+      })
+      .catch(() => {
+      });
   });
 });
