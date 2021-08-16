dd40c8601bda7fe2955edc594adca03d865353e9
yihjian <yihongj2@illinois.edu>
2021-08-15 22:22:27 -0500

    sync working code

--- functions/func/common/upload.ts
+++ functions/func/common/upload.ts
@@ -1,45 +1,23 @@
-/*
 import * as functions from 'firebase-functions';
 import { storage } from '../../db';
-import firebase from 'firebase';
-*/
-/*
-A scratch function that upload file to cloud storage
-input :{
-  postfix: string,
-  ftype: string,
-  file: any,
-  fileName: string,
-}
-written by trn
-TODO:
-1. finished the uplaod function
-*/
 
-/*
-export const upload = async (input :{
-  // postfix: string,
-  ftype: string,
-  file: any, // by using File and Blob API
-  fileName: string,
+export const upload = async (data:{
+  uid : string,
+  fname : string,
+  file : Buffer
 }) => {
-  const { ftype, file , fileName } = input;
-  const storageRef = storage().ref();
-  if (ftype === 'image') {
-    functions.logger.info('here is image uploader');
-    const imagesRef = storageRef.child('image');
-    spaceRef = imagesRef.child(fileName);
-  } else if (ftype === 'video') {
-    const videosRef = storageRef.child('video');
-    functions.logger.info('here is video uploader');
-  } else {
-    functions.logger.info('here is wrong ftype given');
-  }
-  const url = null;
-  // this is root reference
-
-  return url;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { uid, fname, file } = data;
+  console.log(uid + fname);
+  const destFile = await storage.file('fuk/aaabbb');
+  await destFile.save(file, { contentType: 'image/png' }, (err) => {
+    if (!err) {
+      console.log('uploaded');
+      return '0';
+    }
+    console.log(`something wrong${err}`);
+    return '1';
+  });
 };
 
 export default functions.https.onCall(upload);
-*/
--- functions/test/common/upload.t.ts
+++ functions/test/common/upload.t.ts
@@ -1,15 +1,35 @@
-/*
-import { expect } from 'chai';
-import { addVideo } from '../../func/video/addVideoFunc';
-// import { readVideo } from '../../func/video/readVideoFunc';
-// import { deleteVideo } from '../../func/video/deleteVideoFunc';
-// import { updateVideo } from '../../func/video/updateVideoFunc';
+// import { expect } from 'chai';
+
 import 'mocha';
+import fs from 'fs';
+// import https from 'https';
+// import { expect } from 'chai';
+import { upload } from '../../func/common/upload';
+// import { storage } from '../../db';
 // import * as testinfo from '../testInfo/testInfo';
 
 describe('Uplaod File Test', () => {
-  it('up;aod file test', async () => {
-
-  });
+  // it('up;aod file test', async () => {
+  //   const name = await upload({ file: './test/school/test.png' });
+  //   console.log(name);
+  //   const file = await storage.file(name);
+  //   const expiration = new Date();
+  //   expiration.setDate(expiration.getHours() + 7);
+  //   const urls = await file.getSignedUrl({ action: 'read', expires: expiration });
+  //   const tmpFile = fs.createWriteStream('./test/common/tmp.png');
+  //   https.get(urls[0], (response) => {
+  //     if (response.statusCode !== 200) {
+  //       expect.fail('link was not downloadable');
+  //     }
+  //     response.pipe(tmpFile);
+  //     const original = fs.readFileSync('./test/school/test.png');
+  //     const downloaded = fs.readFileSync('./test/common/tmp.png');
+  //     expect(original.compare(downloaded)).to.equal(1);
+  //     fs.unlinkSync('./test/common/tmp.png');
+  //   });
+  // });
+  it ('upload test 2', async () => {
+    await upload({uid:'233', fname:"fjaewio", file:fs.readFileSync('./test/school/test.png')});
+    console.log('?');
+  })
 });
-*/
