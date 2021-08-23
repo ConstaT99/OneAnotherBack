b2e018dddd63a467e2205e3f9190ba10f945b9e2
Catherine Cheng <catherinecheng@Catherines-MacBook-Pro.local>
2021-08-23 12:45:20 -0500

    add comments first draft

---
+++ functions/func/comment/addCommentFunc.ts
@@ -0,0 +1,44 @@
+import * as functions from 'firebase-functions';
+import { db } from '../../db'
+
+/*
+Input {
+    author: the userId of user who composed the comment
+    content: the content of the comment
+    replyToPost: indicates whether the comment is replying to a post or another comment
+    replyId: the id of either a post or another comment (indicated by replyToPost) that this comment is replying to. 
+}
+Output {
+    the comment should be added into the comment collection
+}
+*/
+
+export const addCommentFunc = async (data: {
+    author: string
+    content: string
+    replyToPost: boolean
+    replyId: string
+}) => {
+    const {author, content, replyToPost, replyId} = data;
+    if (content === null) {
+        return Promise.reject(new Error('Empty content, invalid field.'))
+    }
+    const likedBy:Array<string> = []; //list of uids that liked this comment
+    const repliedBy:Array<string> = [] //list of comments that replied to this comment
+    const timePosted:number = Date.now();
+    const commentInfo = {
+        author,
+        content,
+        likedBy,
+        repliedBy,
+        replyId,
+        replyToPost,
+        timePosted,
+    };
+    const collection = 'comment';
+    const commentRef = db.collection(collection);
+    const docRef = await commentRef.add(commentInfo);
+    return docRef.id;
+};
+
+export default functions.https.onCall(addCommentFunc);
