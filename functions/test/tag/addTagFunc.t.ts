9b9e3646bc2bdeb3b997dccb986074b3713b19e5
李东恒 <jamesli@host-208-77.ilcuoct.champaign.il.us.clients.pavlovmedia.net>
2021-08-23 00:09:32 -0500

    finished tag dev

--- functions/test/tag/addTagFunc.t.ts
+++ functions/test/tag/addTagFunc.t.ts
@@ -22,13 +22,14 @@ describe('addTagFunction test', () => {
     it('the result should not be created if the name are the same', async () => {
         const testData = {
             name: '香槟美食',
-            postId: 'abcdefg',
+            postId: 'wR9n7Q6vF5i1kaep0tuq',
         };
         const returnID = await addTagFunc(testData);
         const tagRef = db.collection('tag');
         const docRef = await tagRef.doc(returnID).get();
         //@ts-ignore
-        expect(docRef.data().posts).contain('abcdefg'); 
-        db.collection('tag').doc(returnID).delete();
+        expect(docRef.data().name).to.equal('香槟美食');
+        //@ts-ignore
+        expect(docRef.data().posts).contain('wR9n7Q6vF5i1kaep0tuq'); 
     });
 });
