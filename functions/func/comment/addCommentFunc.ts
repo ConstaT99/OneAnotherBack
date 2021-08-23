582e4020930328e5034b1d4691b6700a87be1a6c
yihjian <yihongj2@illinois.edu>
2021-08-23 13:14:36 -0500

    Fix indent, change time to unix timestamp and update some comments

--- functions/func/comment/addCommentFunc.ts
+++ functions/func/comment/addCommentFunc.ts
@@ -1,12 +1,13 @@
 import * as functions from 'firebase-functions';
-import { db } from '../../db'
+import { db } from '../../db';
 
 /*
+auther @Cath
 Input {
     author: the userId of user who composed the comment
     content: the content of the comment
     replyToPost: indicates whether the comment is replying to a post/comment
-    replyId: the id of the post/comment that this comment is replying to. 
+    replyId: the id of the post/comment that this comment is replying to.
 }
 Output {
     the comment should be added into the comment collection
@@ -16,31 +17,33 @@ Output {
 */
 
 export const addCommentFunc = async (data: {
-    author: string
-    content: string
-    replyToPost: boolean
-    replyId: string
+  author: string
+  content: string
+  replyToPost: boolean
+  replyId: string
 }) => {
-    const {author, content, replyToPost, replyId} = data;
-    if (content === null) {
-        return Promise.reject(new Error('Empty content, invalid field.'))
-    }
-    const likedBy:Array<string> = []; //list of uids that liked this comment
-    const repliedBy:Array<string> = [] //list of comments that replied to this comment
-    const timePosted:number = Date.now();
-    const commentInfo = {
-        author,
-        content,
-        likedBy,
-        repliedBy,
-        replyId,
-        replyToPost,
-        timePosted,
-    };
-    const collection = 'comment';
-    const commentRef = db.collection(collection);
-    const docRef = await commentRef.add(commentInfo);
-    return docRef.id;
+  const {
+    author, content, replyToPost, replyId,
+  } = data;
+  if (content === null) {
+    return Promise.reject(new Error('Empty content, invalid field.'));
+  }
+  const likedBy:Array<string> = []; // list of uids that liked this comment
+  const repliedBy:Array<string> = []; // list of comments that replied to this comment
+  const timePosted:number = Math.floor(Date.now() / 1000);
+  const commentInfo = {
+    author,
+    content,
+    likedBy,
+    repliedBy,
+    replyId,
+    replyToPost,
+    timePosted,
+  };
+  const collection = 'comment';
+  const commentRef = db.collection(collection);
+  const docRef = await commentRef.add(commentInfo);
+  return docRef.id;
 };
 
 export default functions.https.onCall(addCommentFunc);
