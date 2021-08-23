50c50b7345cd784a028221faa3c4e58b51eb0d39
李东恒 <jamesli@host-208-77.ilcuoct.champaign.il.us.clients.pavlovmedia.net>
2021-08-23 12:29:16 -0500

    all func tested

--- functions/test/common/delete.t.ts
+++ functions/test/common/delete.t.ts
@@ -1,9 +1,9 @@
 import 'mocha';
+import { expect } from 'chai';
 import {deleteFile} from '../../func/common/delete';
 
 describe('delete File Test', () => {
     it('delete file test 1', async () => {
-        const testDelete = await deleteFile({uid: 'test', fname: 'test.jpg'});
-        console.log(testDelete);
+        await deleteFile({uid: 'test', fname: 'test.jpg'}).then(()=>{}).catch(()=>expect.fail());
     });
 });
