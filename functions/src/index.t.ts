f9fa295fee6a1ce7c64b9a70eac4cae0c8691af2
james_carstin <carstin@wirelessprv-10-192-93-94.near.illinois.edu>
2021-08-24 14:40:46 -0500

    cat done

--- functions/src/index.t.ts
+++ functions/src/index.t.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/export */
 
-export * from '../test/template/addReadExample.t';
+//export * from '../test/template/addReadExample.t';
 
 export * from '../test/user/userReadTest';
 export * from '../test/user/userUpdateTest';
