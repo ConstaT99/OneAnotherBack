a21e5da52efe9db836a9e8757152940bac7d3d97
Tao, Jerry <rtao6@illinois.edu>
2021-08-29 21:32:44 -0500

    working on the test

--- functions/src/index.t.ts
+++ functions/src/index.t.ts
@@ -1,20 +1,22 @@
 /* eslint-disable import/export */
-// User collection
-export * from '../test/user/userReadTest';
-export * from '../test/user/userUpdateTest';
+
+// // User collection
+// export * from '../test/user/userReadTest';
+// export * from '../test/user/userUpdateTest';
 
 // Post collection
 export * from '../test/post/postReadTest.t';
+export * from '../test/post/readPostTest.t';
 
-// School collection
-export * from '../test/school/schoolTest.t';
+// // School collection
+// export * from '../test/school/schoolTest.t';
 
-// tag collection
-export * from '../test/tag/isTagExists.t';
-export * from '../test/tag/addTagFunc.t';
+// // tag collection
+// export * from '../test/tag/isTagExists.t';
+// export * from '../test/tag/addTagFunc.t';
 
-// categories
-export * from '../test/categories/updateCatFunc.t';
-export * from '../test/categories/deletePostFromCat.t';
-export * from '../test/categories/isCatExists.t';
-export * from '../test/categories/updateCatFunc.t';
+// // categories
+// export * from '../test/categories/updateCatFunc.t';
+// export * from '../test/categories/deletePostFromCat.t';
+// export * from '../test/categories/isCatExists.t';
+// export * from '../test/categories/updateCatFunc.t';
