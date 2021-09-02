3877fbe5083c62532e862dea0073da2e923fd685
李东恒 <jamesli@host-208-77.ilcuoct.champaign.il.us.clients.pavlovmedia.net>
2021-09-01 22:43:32 -0500

    uploadMultiple v1

--- functions/func/common/uploadMultipleFileFunc.ts
+++ functions/func/common/uploadMultipleFileFunc.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import { getFileUrl } from './getFileUrlFunc';
 import { uploadFile } from './uploadFileFunc';
 
 
@@ -8,16 +9,17 @@ export const uploadMultipleFile = async (data:{
     file : Buffer[]
 }) => {
     const { uid, fname, file } = data;
-    if (file.length === 0) {
-        return Promise.reject('file array is empty');
+    if (file.length === 0 || fname.length === 0) {
+        return Promise.reject('file array or fileName array is empty');
     }
-    var destNameArray = [];
+    var urlArray = [];
     for (let i = 0; i < file.length; i++) {
         var tmpFname = fname[i];
         var tmpFile = file[i];
         var tmpDestName = await uploadFile({uid: uid, fname: tmpFname, file: tmpFile});
-        destNameArray.push(tmpDestName);
+        var url = getFileUrl({file: tmpDestName});
+        urlArray.push(url);
     }
-    return destNameArray;
+    return urlArray;
 }
 export default functions.https.onCall(uploadMultipleFile);
