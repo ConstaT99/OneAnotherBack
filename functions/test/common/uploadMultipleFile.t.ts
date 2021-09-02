77d0f3e4f3d92c022d024f42235d517d4543143b
李东恒 <jamesli@host-208-77.ilcuoct.champaign.il.us.clients.pavlovmedia.net>
2021-09-02 00:02:19 -0500

    uploadMultiple func

--- functions/test/common/uploadMultipleFile.t.ts
+++ functions/test/common/uploadMultipleFile.t.ts
@@ -4,11 +4,10 @@ import { uploadMultipleFile } from '../../func/common/uploadMultipleFileFunc';
 
 describe('Upload multiple file Test', () => {
   it('upload multiple file test', async () => {
-    const fileRef = await uploadMultipleFile({
+    await uploadMultipleFile({
       uid: 'test',
       fname: ['test.jpg', '肌肉泽昊.mp4'],
       file: [fs.readFileSync('./test/testInfo/live_and_create.jpg'), fs.readFileSync('./test/testInfo/肌肉泽昊.mp4')],
     });
-    console.log(fileRef);
   });
 });
