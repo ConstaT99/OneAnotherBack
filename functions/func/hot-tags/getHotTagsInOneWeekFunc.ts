8f90d52be37957f0ce52662f307b7045d440dc94
李东恒 <jamesli@host-208-77.ilcuoct.champaign.il.us.clients.pavlovmedia.net>
2021-09-04 00:37:30 -0500

    getTenHotTagsFunc update getHotTagsInOneMonth getHotTagsInOneWeek getHotTagsInOneDay added

--- functions/func/hot-tags/getHotTagsInOneWeekFunc.ts
+++ functions/func/hot-tags/getHotTagsInOneWeekFunc.ts
@@ -13,31 +13,31 @@ Output {
 */
 
 export const getHotTagsInOneWeek = async () => {
-    const currenttime = Math.floor(Date.now() / 1000);
-    const time = currenttime - 604800;
-    const tagCollection = db.collection('tag');
-    const snapshot = await tagCollection.where('lastUpdate', '>=', time).get();
-    const arrayOfTag = snapshot.docs.map((doc) => doc.data());
-    const sortedArrayOfTag = arrayOfTag.sort((n1, n2) => { // sort the Array in desc order
+  const currenttime = Math.floor(Date.now() / 1000);
+  const time = currenttime - 604800;
+  const tagCollection = db.collection('tag');
+  const snapshot = await tagCollection.where('lastUpdate', '>=', time).get();
+  const arrayOfTag = snapshot.docs.map((doc) => doc.data());
+  const sortedArrayOfTag = arrayOfTag.sort((n1, n2) => { // sort the Array in desc order
     if (n1.tagScore > n2.tagScore) {
-        return -1;
+      return -1;
     }
     if (n1.tagScore < n2.tagScore) {
-        return 1;
+      return 1;
     }
     return 0;
-    });
-    const arraylength = sortedArrayOfTag.length;
-    if (arraylength >= 20) {
-        const outArray = [];
-        let i = 0;
-        while (i < 20) {
-          outArray.push(sortedArrayOfTag[i]);
-          i += 1;
-        }
-        return outArray;
-      }
-    return sortedArrayOfTag;
-}
+  });
+  const arraylength = sortedArrayOfTag.length;
+  if (arraylength >= 20) {
+    const outArray = [];
+    let i = 0;
+    while (i < 20) {
+      outArray.push(sortedArrayOfTag[i]);
+      i += 1;
+    }
+    return outArray;
+  }
+  return sortedArrayOfTag;
+};
 
 export default functions.https.onCall(getHotTagsInOneWeek);
