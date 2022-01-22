8923b84a5e7356519b182b712362445924818cee
Catherine Cheng <wanningcheng0626@gmail.com>
2022-01-22 17:03:46 -0600

    school names and avatars added

--- functions/func/school/uploadSchoolFunc.ts
+++ functions/func/school/uploadSchoolFunc.ts
@@ -5,6 +5,20 @@ import { uploadFile } from '../../func/common/uploadFileFunc';
 import { getFileUrl } from '../../func/common/getFileUrlFunc';
 import { addSchool } from '../../func/school/addSchoolFunc';
 
+/*
+Author @Cath
+A one-time function used to add all schools' names and avatars into the database.
+Input: {
+  uid: the uid of the corresponding avatar
+  fname: the file name of the avatar
+  fileLocation: the directory of the avatar
+  schoolName: the corresponding school name of the avatar
+}
+Output: {
+  Successfully added the avatars.
+}
+*/
+
 export const uploadSchool = async (data:{
   uid : string,
   fname : string,
@@ -17,7 +31,6 @@ export const uploadSchool = async (data:{
     file: fs.readFileSync(data.fileLocation),
   });
   const url = await getFileUrl({ file: fileRef });
-  console.log(url);
   await addSchool({
       avatar: url,
       schoolName: data.schoolName,
--- functions/test/school/uploadSchool.t.ts
+++ functions/test/school/uploadSchool.t.ts
@@ -1,15 +1,21 @@
-//import { expect } from 'chai';
-
 import 'mocha';
 import { uploadSchool } from '../../func/school/uploadSchoolFunc';
+import { readFileSync } from 'fs';
 
 describe('upload school names and avatars test', () => {
   it('upload school names and avatars test', async () => {
-    await uploadSchool({
-      uid: 'boston_college',
-      fname: 'boston_college.PNG',
-      fileLocation: './schoolinfo/logos/boston_college.PNG',
-      schoolName: '波士顿学院（Boston College）',
-    });
+    const schoolNames = readFileSync('./schoolinfo/names.txt', 'utf8').split('\n');
+    const avatarNames = readFileSync('./schoolinfo/pictureNameIndex.txt', 'utf8').split('\n');
+    const path = './schoolinfo/logos/';
+    let count = 0;
+    for await (var avatarName of avatarNames) {
+      await uploadSchool({
+        uid: avatarName,
+        fname: avatarName+ '.PNG',
+        fileLocation: path + avatarName + '.PNG',
+        schoolName: schoolNames[count],
+      });
+      count+=1;
+    }
   });
 });
